df3dc8f05037ece9d026edaf7c82789af8483a10
makewebme <makewebme@gmail.com>
2020-04-30 22:00:32 +0300

    Final

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express')
 const app = express()
+const mainfest = require('./public/assets-manifest.json')
+
+app.set('view engine', 'ejs')
+
 
 app.use(express.static('public', { maxAge: 60 * 1000 }))
+
+app.use('/', (req, res) => {
+  return res.render('index.ejs', {
+    style: mainfest.style,
+    script: mainfest.script
+  })
+})
+
 
 app.listen(8080, function () {
   console.log('App listening on port 8080')
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,19 @@
+const fs = require('fs')
 const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const RemovePlugin = require('remove-files-webpack-plugin')
+
+
+
+const randomHash = Math.floor(Math.random() * 10000000)
+
+const assetsManifest = JSON.stringify({
+  style: `style.${randomHash}.css`,
+  script: `js/script.${randomHash}.js`
+})
+
+fs.writeFileSync('./public/assets-manifest.json', assetsManifest)
+console.log('Manifest file generated!')
 
 
 
@@ -10,13 +24,24 @@ module.exports = {
   },
 
   output: {
-    filename: `js/[name].js`,
+    filename: `js/[name].${randomHash}.js`,
     path: path.resolve(__dirname, 'public'),
   },
 
   plugins: [
     // Generating CSS
-    new MiniCssExtractPlugin({ filename: `style.css` }),
+    new MiniCssExtractPlugin({ filename: `style.${randomHash}.css` }),
+
+    // Remove generated files
+    new RemovePlugin({
+      before: {
+        root: './public',
+        test: [
+          { folder: './js', method: absPath => new RegExp(/\.js$/).test(absPath) },
+          { folder: '.', method: absPath => new RegExp(/\.css$/).test(absPath) },
+        ]
+      }
+    })
   ],
 
   module: {
